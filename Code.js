435c4b8034d674072f3de0ecb2786c73302c885f
lfk75 <92749457+lfk75@users.noreply.github.com>
2022-04-11 17:06:03 -0300

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  text("Dica: faz uma tarefa!", 700, 170);
+
+  text("Dica: Armazena dados!", 100, 300);
+
+
+
+
+
+}
